38a331e88fe90858b09038752a05a1dfd558d671
Octave Larose <octave.larose@hotmail.fr>
2022-12-19 16:54:25 +0000

    Basic bash script for running benchmarks

---
+++ run_benchmarks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BENCHMARKS=("Bounce" "Mandelbrot" "List" "Permute" "Queens" "QuickSort" "Sieve" "Fannkuch" "Json" "DeltaBlue")
+
+for bench in "${BENCHMARKS[@]}"
+do
+    cargo run --bin som-interpreter-bc -- -c core-lib/Smalltalk core-lib/Examples/Benchmarks core-lib/Examples/Benchmarks/Json -- core-lib/Examples/Benchmarks/BenchmarkHarness.som $bench 1 0 7
+    echo -ne "\n"
+done
+
+
