112709dccbe4ce65a6dd1d0df6de4e1d9c171b52
Octave Larose <octave.larose@hotmail.fr>
2024-01-23 16:49:18 +0000

    making the inlining work properly with shadowing, slowly but surely. step one: working ifTrue inlining for Bounce

--- som-interpreter-bc/src/inliner.rs
+++ som-interpreter-bc/src/inliner.rs
@@ -1,6 +1,7 @@
 use som_core::ast;
 use som_core::bytecode::Bytecode;
-use crate::compiler::{InnerGenCtxt};
+use crate::block::BlockInfo;
+use crate::compiler::{InnerGenCtxt, Literal};
 use crate::compiler::MethodCodegen;
 use crate::inliner::JumpType::{JumpOnFalse, JumpOnTrue};
 
@@ -9,9 +10,12 @@ pub enum JumpType {
     JumpOnTrue
 }
 
+static mut has_been_accessed: bool = false;
+
 // TODO some of those should return Result types and throw errors instead, most likely.
 pub trait PrimMessageInliner {
     fn inline_if_possible(&self, ctxt: &mut dyn InnerGenCtxt, message: &ast::Message) -> Option<()>;
+    fn inline_compiled_block(&self, ctxt: &mut dyn InnerGenCtxt, block: &BlockInfo) -> Option<()>;
     fn inline_expr(&self, ctxt: &mut dyn InnerGenCtxt, block: &ast::Expression) -> Option<()>;
 
     fn inline_if_true_or_if_false(&self, ctxt: &mut dyn InnerGenCtxt, message: &ast::Message, jump_type: JumpType) -> Option<()>;
@@ -23,35 +27,115 @@ impl PrimMessageInliner for ast::Expression {
     fn inline_if_possible(&self, ctxt: &mut dyn InnerGenCtxt, message: &ast::Message) -> Option<()> {
         match message.signature.as_str() {
             "ifTrue:" => self.inline_if_true_or_if_false(ctxt, message, JumpOnFalse),
-            "ifFalse:" => self.inline_if_true_or_if_false(ctxt, message, JumpOnTrue),
-            "ifTrue:ifFalse:" => self.inline_if_true_if_false(ctxt, message, JumpOnFalse),
-            "ifFalse:ifTrue:" => self.inline_if_true_if_false(ctxt, message, JumpOnTrue),
-            "whileTrue:" => self.inline_while(ctxt, message, JumpOnFalse),
-            "whileFalse:" => self.inline_while(ctxt, message, JumpOnTrue),
+            // "ifFalse:" => self.inline_if_true_or_if_false(ctxt, message, JumpOnTrue),
+            // "ifTrue:ifFalse:" => self.inline_if_true_if_false(ctxt, message, JumpOnFalse),
+            // "ifFalse:ifTrue:" => self.inline_if_true_if_false(ctxt, message, JumpOnTrue),
+            // "whileTrue:" => self.inline_while(ctxt, message, JumpOnFalse),
+            // "whileFalse:" => self.inline_while(ctxt, message, JumpOnTrue),
             // TODO: [or, and]
             _ => None
         }
     }
 
-    fn inline_expr(&self, ctxt: &mut dyn InnerGenCtxt, block_expr: &ast::Expression) -> Option<()> {
-        match block_expr {
-            ast::Expression::Block(block) => {
-                for block_local in &block.locals {
-                    ctxt.push_local(String::from(block_local)); // breaks shadowing
-                }
-
-                // TODO need to remove those POPs somehow.
-                if let Some((last, rest)) = block.body.exprs.split_last() {
-                    for expr in rest {
-                        expr.codegen(ctxt);
-                        ctxt.push_instr(Bytecode::Pop);
-                    }
-                    last.codegen(ctxt)?;
-                }
-                Some(())
-            },
-            expr => expr.codegen(ctxt)
+    fn inline_compiled_block(&self, ctxt: &mut dyn InnerGenCtxt, block: &BlockInfo) -> Option<()> {
+        // TODO uncomment
+        for block_local_intern_id in &block.locals {
+            panic!("we don't handle block locals yet!");
+            // ctxt.push_local(ctxt.lookup_symbol(block_local_intern_id));
         }
+
+        // last is always ReturnLocal, so it gets ignored
+        if let Some((_, body)) = block.body.split_last() {
+            for block_bc in body {
+                match block_bc {
+                    Bytecode::PushLocal(up_idx, idx) => ctxt.push_instr(Bytecode::PushLocal(*up_idx - 1, *idx)),
+                    Bytecode::PopLocal(up_idx, idx) => ctxt.push_instr(Bytecode::PopLocal(*up_idx - 1, *idx)),
+                    Bytecode::PushArgument(up_idx, idx) => ctxt.push_instr(Bytecode::PushArgument(*up_idx - 1, *idx)),
+                    Bytecode::PopArgument(up_idx, idx) => ctxt.push_instr(Bytecode::PopArgument(*up_idx - 1, *idx)),
+                    Bytecode::Send1(lit_idx) | Bytecode::Send2(lit_idx) |
+                    Bytecode::Send3(lit_idx) | Bytecode::SendN(lit_idx) => {
+                        match block.literals.get(*lit_idx as usize)? {
+                            Literal::Symbol(interned) => {
+                                // does this push duplicate literals? I think it doesn't?
+                                let idx = ctxt.push_literal(Literal::Symbol(*interned));
+                                match block_bc {
+                                    Bytecode::Send1(_) => ctxt.push_instr(Bytecode::Send1(idx as u8)),
+                                    Bytecode::Send2(_) => ctxt.push_instr(Bytecode::Send2(idx as u8)),
+                                    Bytecode::Send3(_) => ctxt.push_instr(Bytecode::Send3(idx as u8)),
+                                    Bytecode::SendN(_) => ctxt.push_instr(Bytecode::SendN(idx as u8)),
+                                    _ => panic!("Unreachable branch")
+                                }
+                            },
+                            _ => panic!("Unexpected block literal type, not yet implemented")
+                        }
+                    },
+                    // Bytecode::PushBlock(block_idx) => {
+                    // match block.literals.get(*block_idx as usize)? {
+                    //     Literal::Block(inner_block) => {
+                    //         let new_block = compile_block(ctxt.as_gen_ctxt(), &inner_block.ast_body)?;
+                    //         let idx = ctxt.push_literal(Literal::Block(Rc::new(new_block)));
+                    //         ctxt.push_instr(Bytecode::PushBlock(idx as u8));
+                    //     },
+                    //     _ => panic!("PushBlock not actually pushing a block somehow")
+                    // };
+                    // },
+                    Bytecode::PushGlobal(global_idx) => {
+                        match block.literals.get(*global_idx as usize)? {
+                            lit => {
+                                let lit_idx = ctxt.push_literal(lit.clone());
+                                ctxt.push_instr(Bytecode::PushGlobal(lit_idx as u8));
+                            }
+                        };
+                    },
+                    Bytecode::PushConstant(constant_idx) => {
+                        match block.literals.get(*constant_idx as usize)? {
+                            lit => {
+                                let lit_idx = ctxt.push_literal(lit.clone());
+                                match lit_idx {
+                                    0 => ctxt.push_instr(Bytecode::PushConstant0),
+                                    1 => ctxt.push_instr(Bytecode::PushConstant1),
+                                    2 => ctxt.push_instr(Bytecode::PushConstant2),
+                                    _ => ctxt.push_instr(Bytecode::PushConstant(lit_idx as u8))
+                                }
+                            }
+                        };
+                    },
+                    Bytecode::PushConstant0 | Bytecode::PushConstant1 | Bytecode::PushConstant2 => {
+                        let constant_idx: usize = match block_bc {
+                            Bytecode::PushConstant0 => 0,
+                            Bytecode::PushConstant1 => 1,
+                            Bytecode::PushConstant2 => 2,
+                            _ => panic!("Unreachable")
+                        };
+
+                        match block.literals.get(constant_idx)? {
+                            lit => {
+                                let lit_idx = ctxt.push_literal(lit.clone());
+                                match lit_idx {
+                                    0 => ctxt.push_instr(Bytecode::PushConstant0),
+                                    1 => ctxt.push_instr(Bytecode::PushConstant1),
+                                    2 => ctxt.push_instr(Bytecode::PushConstant2),
+                                    _ => ctxt.push_instr(Bytecode::PushConstant(lit_idx as u8))
+                                }
+                            }
+                        };
+                    },
+                    Bytecode::ReturnNonLocal => {ctxt.push_instr(Bytecode::ReturnLocal)},
+                    _ => ctxt.push_instr(*block_bc)
+                }
+            }
+        }
+
+        Some(())
+    }
+
+    #[allow(dead_code)]
+    fn inline_expr(&self, ctxt: &mut dyn InnerGenCtxt, block_expr: &ast::Expression) -> Option<()> {
+        todo!();
+        // match block_expr {
+        //     ast::Expression::Block(block) => self.inline_compiled_block(ctxt, block),
+        //     _ => todo!("does that show up in the tests")//expr.codegen(ctxt)
+        // }
     }
 
     fn inline_if_true_or_if_false(&self, ctxt: &mut dyn InnerGenCtxt, message: &ast::Message, jump_type: JumpType) -> Option<()> {
@@ -59,14 +143,35 @@ impl PrimMessageInliner for ast::Expression {
             return None;
         }
 
+        // we need to compile the block before inlining it, and we haven't encountered/compiled it yet
+        message.values.get(0)?.codegen(ctxt)?;
+
+        let block_idx = match ctxt.get_instructions().last()? {
+            Bytecode::PushBlock(val) => *val,
+            _ => panic!("should be impossible: we've just compiled a block.")
+        };
+        ctxt.pop_instr(); // we remove the PUSH_BLOCK
+
         let jump_idx = ctxt.get_cur_instr_idx();
         match jump_type {
             JumpOnFalse => ctxt.push_instr(Bytecode::JumpOnFalseTopNil(0)),
             JumpOnTrue => ctxt.push_instr(Bytecode::JumpOnTrueTopNil(0))
         }
 
-        // todo i think Recurse took a big hit when i started inlining any expression instead of just blocks. needs investigating
-        self.inline_expr(ctxt, message.values.get(0)?);
+        let cond_block_ref = match ctxt.get_literal(block_idx as usize)? {
+            Literal::Block(val) => val.clone(),
+            _ => return None
+        };
+        // shouldn't break anything, probably
+//        ctxt.remove_literal(block_idx as usize);
+//         dbg!(&cond_block_ref.as_ref().blk_info.body);
+
+        self.inline_compiled_block(ctxt, cond_block_ref.as_ref().blk_info.as_ref());
+
+        // dbg!(ctxt.get_instructions());
+
+//        // todo i think Recurse took a big hit when i started inlining any expression instead of just blocks. needs investigating
+//         self.inline_expr(ctxt, message.values.get(0)?);
         ctxt.backpatch_jump_to_current(jump_idx);
 
         return Some(());
