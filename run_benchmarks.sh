fabf18d28a198d0c701ca503708706afa3b8596c
Octave Larose <octave.larose@hotmail.fr>
2024-03-06 15:54:14 +0000

    block adaptation after inlining: seems functional, actually!

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -4,7 +4,7 @@ BENCHMARKS=("Bounce" "Mandelbrot" "List" "Permute" "Queens" "QuickSort" "Sieve" 
 
 for bench in "${BENCHMARKS[@]}"
 do
-    cargo run --bin som-interpreter-bc -- -c core-lib/Smalltalk core-lib/Examples/Benchmarks core-lib/Examples/Benchmarks/Json core-lib/Examples/Benchmarks/Richards core-lib/Examples/Benchmarks/DeltaBlue -- core-lib/Examples/Benchmarks/BenchmarkHarness.som $bench 1 0 7
+    cargo run --bin som-interpreter-bc -- -c core-lib/Smalltalk core-lib/Examples/Benchmarks core-lib/Examples/Benchmarks/Json core-lib/Examples/Benchmarks/Richards core-lib/Examples/Benchmarks/DeltaBlue -- core-lib/Examples/Benchmarks/BenchmarkHarness.som $bench 1 7
     echo -ne "\n"
 done
 
